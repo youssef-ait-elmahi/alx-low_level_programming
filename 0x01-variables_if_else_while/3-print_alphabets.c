e28531dd22e848f74179f39ae333759b07617510
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-17 10:45:32 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+	char x = 'A';
+	
+	while (i <= 'z')
+{
+		putchar(i);
+		i++;
+}
+	while (x <= 'Z')
+{
+		putchar(x);
+		i++;
+}
+	putchar('\n')
+	return (0);
+}
