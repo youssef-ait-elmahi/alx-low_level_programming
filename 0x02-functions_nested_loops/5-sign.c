9e7ebd4ad4993b4400503ce66500fb7244c59fb1
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-21 13:59:42 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign- checks for lowercase character
+ * @n: parameter
+ * Return: 1 if the 'c' character is lowercase, 0 otherwise
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+{
+		putchar ('+');
+		return (1);
+}
+	else if (n > 0)
+{
+		_putchar ('-');
+		return (-1);
+}
+	else
+{
+		_putchar ('0');
+	return (0);
+}
+}
