a11f55c43389be0c37dbc44bf169d6fe199ed660
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-21 18:09:11 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * time_table- prints the last digit of a number
+ *
+ * Return: (0)
+ */
+void times_table(void)
+	{
+	int x, y, z;
+
+	for (x = 0 ; x < 10 ; i++)
+{
+	for (y = 0 ; y < 10 ; j++)
+{
+	a = x * y;
+	if (y == 0)
+	_putchar(k + '0');
+	if (a < 10 && !y == 0)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(k + '0');
+}
+	if (a >= 10)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(a / 10 + '0');
+	_putchar(a % 10 + '0');
+}
+}
+	_putchar('\n');
+}
+}
