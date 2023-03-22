845bbe2b68f1d15a43f8e6e7fe8b7737589e3f4a
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-21 18:14:16 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,29 @@
  * Return: (0)
  */
 void times_table(void)
-	{
-	int x, y, z;
+{
+	int i, j, k;
 
-	for (x = 0 ; x < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 {
-	for (y = 0 ; y < 10 ; j++)
+	for (j = 0 ; j < 10 ; j++)
 {
-	a = x * y;
-	if (y == 0)
-	_putchar(a + '0');
-	if (a < 10 && !y == 0)
+	k = j * i;
+	if (j == 0)
+	_putchar(k + '0');
+	if (k < 10 && !j == 0)
 {
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(k + '0');
 }
-	if (a >= 10)
+	if (k >= 10)
 {
 	_putchar(',');
 	_putchar(' ');
-	_putchar(a / 10 + '0');
-	_putchar(a % 10 + '0');
+	_putchar(k / 10 + '0');
+	_putchar(k % 10 + '0');
 }
 }
 	_putchar('\n');
-}
-}
