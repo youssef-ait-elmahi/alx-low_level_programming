a11fb3a9287ff48c289fe6664aeae4f61b26239e
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-21 18:41:53 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,72 +1,47 @@
 #include "main.h"
-
 /**
- * printnumber - prints number
- * @nb: nb to be printed
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
-
-void printnumber(int nb)
-{
-	if (nb < 0)
-	{
-		_putchar('-');
-		nb = nb * (-1);
-		printnumber(nb);
-	}
-	else if (nb >= 10)
-	{
-		printnumber(nb / 10);
-		printnumber(nb % 10);
-	}
-	else
-	{
-		_putchar(nb + 48);
-	}
-}
-
-/**
- * print_times_table - prints number multiplication table
- * @n: n the number of multiplication columns
- */
-
 void print_times_table(int n)
 {
-	int i = 0;
-	int j;
-	int num = 0;
+	int i, j, res;
 
-	if (n >= 0 && n < 15)
+	if (!(n > 15 || n < 0))
 	{
-		while (i <= n)
+		for (i = 0; i <= n; i++)
 		{
-			j = 0;
-			while (j < (n + 1))
+			for (j = 0; j <= n; j++)
 			{
-				num = i * j;
-				printnumber(num);
-				if (j < n && i * (j + 1) < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (j < n && i * (j + 1) >= 100)
+				res = (i * j);
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				else if (j < n && i * (j + 1) >= 10)
+				if (res < 10 && j != 0)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar((res % 10) + '0');
 				}
-				j++;
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
 			}
 			_putchar('\n');
-			i++;
 		}
 	}
 }
-
