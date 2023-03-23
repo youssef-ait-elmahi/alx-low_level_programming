4eaf540c6d5aa2097b989dc33cd31f9167c1e151
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-23 11:31:39 -0700

    6. The shortest distance between two points is a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  function that draws a diagonal line on the terminal.
+ *
+ */
+void print_diagonal(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
