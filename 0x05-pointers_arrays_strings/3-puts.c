329a6b29452e2c6298c7dea3c438102b51f86af9
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-27 09:48:20 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
