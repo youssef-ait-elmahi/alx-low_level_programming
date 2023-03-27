837caae4d9adc85c1a1f148150e1e65ab44dd366
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-27 11:33:38 -0700

    6. Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *puts2 -function that prints every other character of a string
+ *@str : string 
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+}
