8bef462e36cdc1d70b7402a0edebe21e665e68e7
youssef ait elmahi <youssefaitelmahi3@gmail.com>
2023-03-27 09:08:29 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,12 @@
  * @s: string to evaluate
  *
  * Return: the length of the string
- */
- int _strlen(char *s)
+*/
+int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+
 	return (i);
 }
