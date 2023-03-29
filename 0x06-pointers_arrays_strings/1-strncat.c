03765a726d45f655c92643c3069040eef1088cf5
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-03-29 21:53:27 +0000

    2. strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,10 +5,25 @@
  * _strcat - concatenates two strings
  * @dest: input parameter string
  * @src: input parameter string
+ * @n: parameter
  * Return: dest
 **/
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-    
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j] && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest = 0;
+	return (dest);
 }
