e3be47c5fa1c688dcc5238214d8d6e4fb5ecd133
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-03-29 23:13:13 +0000

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
 *@s1: first string
 *@s2:second string
 *Return:
-*        returns zero if s1 == s2
-*        returns negative number if s1 < s2
-*        returns positive number if s1 > s2
+* returns zero if s1 == s2
+* returns negative number if s1 < s2
+* returns positive number if s1 > s2
 */
 int _strcmp(char *s1, char *s2)
 {
