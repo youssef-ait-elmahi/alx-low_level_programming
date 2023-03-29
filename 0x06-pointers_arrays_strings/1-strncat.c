cfa2890612a7c7b185837c1e0fdd732d19e3269c
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-03-29 16:47:53 +0000

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ * Return: dest
+**/
+
+char *_strcat(char *dest, char *src)
+{
+    
+}
