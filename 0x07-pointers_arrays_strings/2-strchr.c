7983fcd8e56a029a641fcae3a4c1d59ed20ea66b
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-03 16:20:52 +0000

    _strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
 
-	while (s[i])
+	while (s)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (s[i]);
+			return (s);
 		}
-		i++;
+		s++;
 	}
-	else
-		return ('\0');
+	else if (c == '\0')
+		return (s);
+	return ('\0');
 }
