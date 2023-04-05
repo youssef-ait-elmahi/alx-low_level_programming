9c945c772648b63b236244af51c1b2828857decb
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-05 14:16:45 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_recursion - function that prints a string, followed by a new line.
+ * @s : string
+ * Return: string
+*/
+void _print_recursion(char *s)
+{
+	if (*s  == '\0')
+	{
+		return;
+		_putchar('\n')
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
