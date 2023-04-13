33b4d09b99558d86ec0f6a7e073bf54fb11ba15f
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-13 18:37:30 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: the buffer in the memory
+ * @b: the character to fill the n bytes of memory
+ * @n: the len of memory to be filled using _memset
+ * Return: (s).
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
 
 /**
- * array_range -  function that allocates memory for an array
- * @min: the starting value "included"
- * @max: the last value "excluded"
- * Return: a pointer to array, NULL otherwise
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: the number of elements
+ * @size: the size of each element
+ * Return: if everything works ptr, NULL otherwise.
  */
 
-int *array_range(int min, int max)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array;
-	int i;
+	void *ptr;
 
-	i = 0;
-	if (min > max)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	array = malloc(sizeof(int) * (max - min + 1));
-	if (!array)
+	ptr = malloc(nmemb * size);
+	if (!ptr)
 		return (NULL);
-	while (min <= max)
-	{
-		array[i] = min;
-		min++;
-		i++;
-	}
-	return (array);
+	__memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
