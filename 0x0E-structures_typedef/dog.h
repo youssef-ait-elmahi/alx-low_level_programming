0c886c080e2288dcae312fc1b030f02d6c255bc2
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-17 22:38:55 +0000

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,11 @@ struct dog
 	char *owner;
 };
 /**
- * dog_t - new type dog_t as a new name for the type struct dog.
+ * dog_t - new type dog_t as a new name for the type struct dog
 */
 typedef struct dog dog_t;
 
-
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
