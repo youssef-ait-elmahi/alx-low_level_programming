d28fd7d1098d7089005a6ab39fbf078485d15741
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-19 16:03:16 +0000

    0. What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCPTR_H
 #define FUNCPTR_H
 #include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
