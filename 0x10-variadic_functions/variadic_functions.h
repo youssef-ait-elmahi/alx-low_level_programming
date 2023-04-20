8a4502d635aa97fcbb6b81cc09fd06e9ab106078
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-20 16:22:13 +0000

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,8 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARF_H
+#define VARF_H
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
