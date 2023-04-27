56c45c61449c49fec445b20704f95e438fae7291
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-27 20:12:22 +0100

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,13 @@
  */
 void free_list(list_t *head)
 {
-    list_t *current;
+	list_t *current;
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
