c23a7bc4899cd80fdd540b40dbe94f864ebf5702
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-04-27 20:09:30 +0100

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head
+ */
+void free_list(list_t *head)
+{
+    list_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current->str);
+        free(current);
+    }
+}
