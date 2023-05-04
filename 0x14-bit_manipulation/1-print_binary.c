d8fdff25a4e44c4d97b26d32ec6e4f62b81b8ae6
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-05-04 19:32:11 +0100

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,14 @@
  */
 void print_binary(unsigned long int n)
 {
-    int len = sizeof(n) * 8;
-    int i;
+	int len = sizeof(n) * 8;
+	int i;
 
-    for (i = len - 1; i >= 0; i--)
-    {
-        if ((n >> i) & 1)
-            _putchar('1');
-        else
-            _putchar('0');
-    }
+	for (i = len - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
 }
