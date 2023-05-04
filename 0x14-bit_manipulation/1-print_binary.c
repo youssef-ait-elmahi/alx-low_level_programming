1db302eee447fa67d61fb0ab45b84c5ea659caa4
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-05-04 19:20:44 +0100

    1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+/**
+ * print_binary - prints the binary representation of an unsigned long int
+ * @n: number to print in binary
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+    unsigned long int i = 1;
+
+    while (i <= n / 2)
+        i <<= 1;
+
+    while (i > 0)
+    {
+        if (n & i)
+            _putchar('1');
+        else
+            _putchar('0');
+        i >>= 1;
+    }
+    _putchar('\n');
+}
