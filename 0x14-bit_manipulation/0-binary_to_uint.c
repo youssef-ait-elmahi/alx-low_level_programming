a9b95fe0c4ec5abf104e8b5cf0d6e2886b96e24e
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-05-04 19:09:03 +0100

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,13 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-
 	for (; *b != '\0'; b++)
 	{
 		if (*b < '0' || *b > '1')
 			return (0);
-
 		num <<= 1;
 		if (*b == '1')
 			num += 1;
 	}
-
 	return (num);
 }
