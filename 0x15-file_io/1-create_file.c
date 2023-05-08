fe8abd5f71918e44bfa7928891f961d2fc6089f3
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-05-08 21:49:14 +0100

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * create_file - Create a function that creates a file
+ * create_file - function that creates a file
  * @filename: name of the file to create
  * @text_content: NULL-terminated string to write to the file
  * Return: 1 on success, -1 on failure
