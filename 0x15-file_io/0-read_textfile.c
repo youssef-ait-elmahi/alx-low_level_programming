db9e0dc6986d94fb0c29199b49a19cb8a3962fce
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-05-08 21:05:15 +0100

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * read_textfile -function that reads a text file and prints
+ *									it to the POSIX standard output
+ * @filename: Name of the file to read.
+ * @letters: Number of letters to read and print.
+ * Return: Actual number of letters read and printed, or 0 if an error occurs
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t tu;
+	ssize_t wr;
+	ssize_t funcd;
+	char *buffer;
+
+	funcd = open(filename, O_RDONLY);
+
+	if (funcd == -1)
+	{
+		return (0);
+	}
+	
+	buffer = malloc(sizeof(char) * letters);
+
+	tu = read(funcd, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, tu);
+
+	free(buffer);
+	close(funcd);
+	return (wr);
+}
