ece7ebe15779dc2c5cf510ab1c5e00dff6bc088d
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-06-15 20:40:55 +0100

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t node_count = 0;
+	const dlistint_t *current = h;
+	size_t node_count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        node_count++;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		node_count++;
+	}
 
-    return node_count;
+	return (node_count);
 }
